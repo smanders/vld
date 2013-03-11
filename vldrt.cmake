ee598209922152c8cf2435463e90a348d10055e1
Scott M Anderson <scott.nosredna@gmail.com>
2013-03-11 15:44:48 -0600

    vld runtime cmake file

---
+++ vldrt.cmake
@@ -0,0 +1,16 @@
+if(WIN32)
+  if(${CMAKE_GENERATOR} MATCHES "Win64$")
+    set(lib_DIR ${externpro_DIR}/lib64)
+    set(dll_suffix "_x64")
+  else()
+    set(lib_DIR ${externpro_DIR}/lib32)
+    set(dll_suffix "_x86")
+  endif()
+  set(vld_runtime
+    debug ${lib_DIR}/dbghelp.dll
+    debug ${lib_DIR}/Microsoft.DTfW.DHL.manifest
+    debug ${lib_DIR}/vld${dll_suffix}.dll
+    )
+else()
+  set(vld_runtime)
+endif()
